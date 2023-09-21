e951228beeba65ad82d5c7ddefa85e2476637470
AdvayArgade <143066312+AdvayArgade@users.noreply.github.com>
2023-09-21 22:37:46 +0530

    RFE and Information Gain intersection

--- PredictingCritTemp.R
+++ PredictingCritTemp.R
@@ -10,6 +10,7 @@ library(FSelectorRcpp)
 library(car)
 library(MASS)
 library(brms)
+library(FSelector)
 #Min max scaling----
 normalize<-function(x){
   if (is.vector(x)) {
@@ -29,14 +30,51 @@ df = cbind(as.data.frame(lapply(df, normalize)))
 
 
 # Feature selection----
-# information gain 
+# information gain----
 # critical_temp = df$critical_temp
 # gains = information_gain(x = subset(df, select = -critical_temp), y= df$critical_temp, type = 'infogain', equal = TRUE)
-# top_attrs = cut_attrs(attrs = gains, k = 0.2)
+# top_attrs = cut_attrs(attrs = gains, k = 0.4)
 # df = subset(df, select = top_attrs)
 # df = cbind(df, critical_temp)
 
+#fischer score----
+# att<-df[,1:80]
+# class_labels<-df$critical_temp
+# data_df<-data.frame(att,Class=class_labels)
+# fs_score<-information.gain(Class~.,data=data_df)
+# print(fs_score)
+# top_attrs_fs = cut_attrs(fs_score, k=0.2)
+#sorted_fs_score <- fs_score[order(fs_score)]
+#print(sorted_fs_score)
+
+
+#lasso----
+# your_data <- read.csv('train.csv')
+# X <- your_data[, 1:80]  
+# y <- your_data$critical_temp 
+# library(glmnet)
+# lasso_model <- glmnet(X, y, alpha = 1) 
+# optimal_lambda <- lasso_model$lambda.min
+# lasso_model_optimal <- glmnet(X, y, alpha = 1, lambda = optimal_lambda)
+# lasso_coefficients <- coef(lasso_model_optimal)
+# selected_features <- which(lasso_coefficients != 0)
+# print(selected_features)
+#CFS----
+#failed
+# library(caret)
+# library(mlbench)
+# data <- read.csv('train.csv')
+# y<-data$critical_temp
+# x<-subset(data,select = -c(y))
+# cfs_feature<-findCorrelation(cor(x),cutoff = 0.5)
+# print(cfs_feature)
+
+
+# Get the optimal feature subset
+top5attrs = intersect(results$optVariables[1:30], top_attrs[1:30])
+
 # Train test split----
+# All features
 set.seed(42)
 split = sample.split(df, SplitRatio = 0.8)
 train = subset(df, split==TRUE)
@@ -47,39 +85,78 @@ y_train = as.matrix(subset(train, select = critical_temp))
 y_test = subset(test, select = critical_temp)
 y_test <- test$critical_temp
 
+# Top 5 features
+
+x_train_top = x_train[, top5attrs]
+x_test_top = x_test[, top5attrs]
 
 # RFE
+# y_train = train$critical_temp
+# 
 # set.seed(42)
-# # control = rfeControl(functions= rfFuncs, method="cv", number=3)
-# results <- rfe(x = x_train, y = y_train,
-#                sizes=c(20:50), metric = "Rsquared", maximize=TRUE)
+# control = rfeControl(functions= rfFuncs, method="cv", number = 3)
+# results <- rfe(x_train, y_train,
+#                c(25:55), "Rsquared", maximize=TRUE, rfeControl = control)
 # # summarize the results
 # print(results)
+
+
+
 
 # print(head(x_test))
 # print(head(y_test))
 
 # Regression----
-folds = createFolds(y_train, k = 5)
-xgb_train = xgb.DMatrix(data = x_train, label = y_train)
-xgb_test = xgb.DMatrix(data = x_test, label = y_test)
-watchlist = list(train=xgb_train, test=xgb_test)
-start_time = Sys.time()
-regressor = xgb.train(data = xgb_train, nrounds = 200, max.depth = 5, watchlist = watchlist
-                      , eta = 0.285, lambda = 1.01)
-
-end_time = Sys.time()
+# XGBoost----
+# folds = createFolds(y_train, k = 5)
+# cat("All features:")
+# xgb_train = xgb.DMatrix(data = x_train, label = y_train)
+# xgb_test = xgb.DMatrix(data = x_test, label = y_test)
+# watchlist = list(train=xgb_train, test=xgb_test)
+# start_time = Sys.time()
+# regressor = xgb.train(data = xgb_train, nrounds = 200, max.depth = 5, watchlist = watchlist
+#                       , eta = 0.285, lambda = 1.01)
+# 
+# end_time = Sys.time()
+# print(end_time-start_time)
+# y_pred = predict(regressor, xgb_test)
+# standard_error = RMSE(y_pred = y_pred, y_true = y_test)
+# lower_interval <- y_pred - 1.96 * standard_error
+# upper_interval <- y_pred + 1.96 * standard_error
+# predictions = data.frame(cbind(y_pred, lower_interval, upper_interval))
+# print(head(predictions))
+# 
+# print(MAE(y_pred = y_pred, y_true = y_test))
+# print(R2(pred = y_pred, obs = y_test))
+# print(RMSE(y_pred = y_pred, y_true = y_test))
+# 
+# cat("\nTop 5 features:")
+# xgb_train = xgb.DMatrix(data = x_train_top, label = y_train)
+# xgb_test = xgb.DMatrix(data = x_test_top, label = y_test)
+# watchlist = list(train=xgb_train, test=xgb_test)
+# start_time = Sys.time()
+# regressor = xgb.train(data = xgb_train, nrounds = 200, max.depth = 5, watchlist = watchlist
+#                       , eta = 0.285, lambda = 1.01)
+# 
+# end_time = Sys.time()
+# print(end_time-start_time)
+# y_pred_top = predict(regressor, xgb_test)
+# standard_error = RMSE(y_pred = y_pred, y_true = y_test)
+# lower_interval <- y_pred_top - 1.96 * standard_error
+# upper_interval <- y_pred_top + 1.96 * standard_error
+# predictions = data.frame(cbind(y_pred_top, lower_interval, upper_interval))
+# print(head(predictions))
+# 
+# print(MAE(y_pred = y_pred_top, y_true = y_test))
+# print(R2(pred = y_pred_top, obs = y_test))
+# print(RMSE(y_pred = y_pred_top, y_true = y_test))
 #Anova(regressor)
 # while(max(Anova(regressor)$'Pr(>F)') > 0.05) {
 #   remove <- names(which.max(Anova(regressor)$'Pr(>F)'))
 #   regressor <- update(regressor, . ~ . - remove)
 # }
 
-#print(end_time-start_time)
-# y_pred = predict(regressor, xgb_test)
-# print(MAE(y_pred = y_pred, y_true = y_test))
-# print(R2(pred = y_pred, obs = y_test))
-# print(RMSE(y_pred = y_pred, y_true = y_test))
+
 
 
 # Bayesian NN----
@@ -96,8 +173,13 @@ end_time = Sys.time()
 
 
 # ANN----
-# library(h2o)
-# h2o.init(nthreads = -1)
+critical_temp = train$critical_temp
+train_top = cbind(x_train_top, critical_temp)
+critical_temp = test$critical_temp
+test_top = cbind(x_test_top, critical_temp)
+library(h2o)
+h2o.init(nthreads = -1)
+# All features----
 # model = h2o.deeplearning(standardize = FALSE,
 #                         y = 'critical_temp',
 #                          training_frame = as.h2o(train),
@@ -114,6 +196,28 @@ end_time = Sys.time()
 # print(MAE(y_pred = y_pred, y_true = y_test))
 # print(R2(pred = y_pred, obs = y_test))
 # print(RMSE(y_pred = y_pred, y_true = y_test))
+
+
+# Top 5 features----
+start_time = Sys.time()
+model = h2o.deeplearning(standardize = FALSE,
+                         y = 'critical_temp',
+                         training_frame = as.h2o(train_top),
+                         validation_frame = as.h2o(test_top),
+                         activation = 'RectifierWithDropout',
+                         hidden = c(100,100, 75),
+                         epochs = 100,
+                         loss = "Absolute",
+                         verbose = TRUE,
+                         train_samples_per_iteration = -2)
+
+end_time = Sys.time()
+print(end_time - start_time)
+y_pred = h2o.predict(model, as.h2o(x_test_top))
+y_pred = as.vector(as.numeric(y_pred))
+print(MAE(y_pred = y_pred, y_true = y_test))
+print(R2(pred = y_pred, obs = y_test))
+print(RMSE(y_pred = y_pred, y_true = y_test))
 
 
 
