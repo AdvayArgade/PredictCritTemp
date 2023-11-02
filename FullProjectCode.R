b12edf90c4820f660e666aee85f91b6997db5a48
AdvayArgade <143066312+AdvayArgade@users.noreply.github.com>
2023-11-02 17:58:59 +0530

    Almost fully working. Just needs some cleanup.

--- FullProjectCode.R
+++ FullProjectCode.R
@@ -14,6 +14,7 @@ library(FSelector)
 library(randomForest)
 library(earth)
 library(e1071)
+library(gbm)
 #Min max scaling----
 normalize<-function(x){
   if (is.vector(x)) {
@@ -39,7 +40,7 @@ df = cbind(as.data.frame(lapply(df, normalize)))
 # top_attrs = cut_attrs(attrs = gains, k = 0.99)
 # df = subset(df, select = top_attrs)
 # df = cbind(df, critical_temp)
-top_attrs = readRDS(' InfoGainResult.rds')
+
 
 #fischer score----
 
@@ -69,28 +70,34 @@ cat("\nSelected Features:\n", FS_features, sep = "\n")
 library(readr)
 
 library(caret)
-set.seed(42)
-index<-createDataPartition(df$critical_temp,p=.8,list=FALSE,times=1)
-df<-as.data.frame(df)
-train_df<-df[index,]
-test_df<-df[-index,]
-#cross validation
-ctrlspecs<-trainControl(method ="cv",number=10,savePredictions = "all")
-lambda_vector<- 10^seq(5,-5,length=500)
-model1<-train(critical_temp~.,
-              data = train_df,
-              preProcess=c("center","scale"),
-              method="glmnet",
-              tuneGrid=expand.grid(alpha=1, lambda=lambda_vector),
-              na.action = na.omit)
-model1$bestTune
-model1$bestTune$lambda
-#lasso reg ceof
-coefficient<-round(coef(model1$finalModel, model1$bestTune$lambda),3)
-#print(coefficient)
-abs_coef<-abs(coefficient)
-ordered_coef<-abs_coef[order(-abs_coef[,]),]
-print(ordered_coef)
+library(glmnet)
+library(gbm)
+
+df_scaled <- as.data.frame(lapply(df, normalize))
+
+# Feature selection using Lasso
+att <- as.matrix(df_scaled[, 1:80])
+class_labels <- df_scaled$critical_temp
+
+# Find the best lambda
+cv_model <- cv.glmnet(att, class_labels, alpha = 1)
+best_lambda <- cv_model$lambda.min
+
+# Train Lasso model with the best lambda
+lasso_model <- glmnet(att, class_labels, alpha = 1, lambda = best_lambda)
+
+# Extract coefficients
+coefficients <- coef(lasso_model)
+abs_coefficients <- abs(coefficients)
+
+important_features <- which(abs_coefficients > 0)
+
+# Sort important features by absolute coefficient magnitude
+sorted_important_features <- important_features[order(-abs_coefficients[important_features])]
+
+LASSO_features <- rownames(coefficients)[sorted_important_features[1:10]]
+
+
 # #var imp---gives top 20 features----
 # varImp(model1)
 
@@ -112,18 +119,6 @@ print(ordered_coef)
 # top_attrs = cut_attrs(attrs = gains, k = 0.99)
 # saveRDS(top_attrs, file = "InfoGainResult.rds")
 
-
-# Train test split----
-# All features
-set.seed(42)
-split = sample.split(df, SplitRatio = 0.8)
-train = subset(df, split==TRUE)
-test = subset(df, split==FALSE)
-x_train = as.matrix(subset(train, select = -critical_temp))
-x_test = as.matrix(subset(test, select = -critical_temp))
-y_train = as.matrix(subset(train, select = critical_temp))
-y_test = subset(test, select = critical_temp)
-y_test <- test$critical_temp
 
 
 
@@ -163,18 +158,20 @@ for (i in 1:nrow(highly_correlated_pairs)) {
 
 # Create a new data frame with the excluded columns
 df_filtered <- df[, !names(df) %in% columns_to_exclude]
-print(df_filtered)
-print(ncol(df_filtered))
-print(nrow(df_filtered))
-print(colnames(df_filtered))
-data = df_filtered
-data = normalize(data)
-# Split the data into features (X) and target variable (y)
-X <- data[, -ncol(data)]  # All columns except the last one
-y <- data[, ncol(data)]   # Last column
+CM_features = colnames(df_filtered)
 
 
-
+# Train test split----
+# All features
+set.seed(42)
+split = sample.split(df, SplitRatio = 0.8)
+train = subset(df, split==TRUE)
+test = subset(df, split==FALSE)
+x_train = as.matrix(subset(train, select = -critical_temp))
+x_test = as.matrix(subset(test, select = -critical_temp))
+y_train = as.matrix(subset(train, select = critical_temp))
+y_test = subset(test, select = critical_temp)
+y_test <- test$critical_temp
 
 
 
@@ -225,53 +222,15 @@ plot <- ggplot(data = eval_df, aes(x = Round)) +
     title = "XGBoost Training Progress with RMSE Metric"
   ) +
   theme_minimal()
+print(plot)
 
-num_features <- c(1:20)
-# Plot R2
-p = plot(num_features, r2, type = "b", pch = 19, col = "blue", xlab = "Number of Features", ylab = "R2")
-# Add connecting lines between points
-lines(num_features, rmse, col = "blue")
-# Add a grid for better visualization (optional)
-grid()
-print(p)
 
-# Plot RMSE
-p = plot(num_features, rmse, type = "b", pch = 19, col = "orange", xlab = "Number of Features", ylab = "RMSE")
-# Add connecting lines between points
-lines(num_features, rmse, col = "orange")
-# Add a grid for better visualization (optional)
-grid()
-print(p)
-
-# Plotting MAE vs number of features
-p = plot(num_features, mae, type = "b", pch = 19, col = "forestgreen", xlab = "Number of Features", ylab = "MAE")
-# Add connecting lines between points
-lines(num_features, mae, col = "forestgreen")
-# Add a grid for better visualization (optional)
-grid()
-print(p)
-
-# Plot training time
-p = plot(num_features, training_time, type = "b", pch = 19, col = "green", xlab = "Number of Features", ylab = "Training Time")
-# Add connecting lines between points
-lines(num_features, training_time, col = "green")
-# Add a grid for better visualization (optional)
-grid()
-print(p)
-
-# Plot prediction time
-p = plot(num_features, pred_time, type = "b", pch = 19, col = "cyan", xlab = "Number of Features", ylab = "Prediction Time")
-# Add connecting lines between points
-lines(num_features, pred_time, col = "cyan")
-# Add a grid for better visualization (optional)
-grid()
-print(p)
 
 # ANN----
 critical_temp = train$critical_temp
-train_top = cbind(x_train_top, critical_temp)
+train = cbind(x_train, critical_temp)
 critical_temp = test$critical_temp
-test_top = cbind(x_test_top, critical_temp)
+test = cbind(x_test, critical_temp)
 library(h2o)
 h2o.init(nthreads = -1)
 # All features----
@@ -324,7 +283,7 @@ cat("\n\nTraining Time:", T1)
 # Print the model summary
 print(RFM)
 
-# Model Accuracy
+# Model Performance 
 T2 <- system.time({Temp_pred <- predict(RFM, test)})
 result <- data.frame(test$critical_temp, Temp_pred)
 cat("\n\nPrediction Time:", T2)
@@ -347,7 +306,7 @@ cat("\nMAE:", mae)
 
 # Build a MARS model using the training data
 T1 <- system.time({
-  mars_model <- earth(critical_temp ~ ., data = train_data)
+  mars_model <- earth(critical_temp ~ ., data = train)
 })
 cat("Training Time:", T1)
 
@@ -356,56 +315,65 @@ print(mars_model)
 
 # Make predictions on the testing set
 T2 <- system.time({
-  predictions <- predict(mars_model, newdata = test_data)
+  predictions <- predict(mars_model, newdata = test)
 })
 print(predictions)
 cat("Prediction Time:", T2)
 
 # Calculate performance metrics (e.g., RMSE) for the predictions
-RMSE <- rmse(predictions, test_data$critical_temp)
+RMSE <- RMSE(predictions, test$critical_temp)
 cat("\nRoot Mean Squared Error (RMSE):", RMSE)
 
-MAE <- mae(predictions, test_data$critical_temp)
+MAE <- MAE(predictions, test$critical_temp)
 cat("\nMAE:", MAE)
 
-r_square <- R2(predictions, test_data$critical_temp)
+r_square <- R2(predictions, test$critical_temp)
 cat("\nR2:", r_square)
 
 
 # SVR----
+x_train = as.matrix(subset(train, select = -critical_temp))
+x_test = as.matrix(subset(test, select = -critical_temp))
+y_train = as.matrix(subset(train, select = critical_temp))
+y_test = subset(test, select = critical_temp)
+y_test <- test$critical_temp
 # Training SVR model
+set.seed(42)
 start_time <- Sys.time()
 
-svr_model <- svm(y_train ~ ., data = data.frame(cbind(y_train, X_train)), kernel = "radial", cost = 1)
+svr_model <- svm(y_train ~ ., data = data.frame(cbind(y_train, x_train)), kernel = "radial", cost = 1)
 
 end_time <- Sys.time()
 # Predict on the test set
-y_pred <- predict(svr_model, newdata = data.frame(X_test))
+y_pred <- predict(svr_model, newdata = x_test)
 
 
 # Print the results
-cat("R-squared (R2): ", r2, "\n")
-cat("Root Mean Squared Error (RMSE): ", rmse, "\n")
-cat("Mean Absolute Error (MAE): ", mae, "\n")
+cat("R-squared (R2): ", R2(y_pred, y_test), "\n")
+cat("Root Mean Squared Error (RMSE): ", RMSE(y_pred, y_test), "\n")
+cat("Mean Absolute Error (MAE): ", MAE(y_pred, y_test), "\n")
 elapsed_time <- end_time - start_time
 cat("\nTime taken: ", elapsed_time, " seconds\n")
 
 
 
 
-results = readRDS('rfeResult.rds')
-top_attrs = readRDS('InfoGainResult.rds')
+RFE_features = readRDS('rfeResult.rds')
+InfoGain_features = readRDS('InfoGainResult.rds')
 # Get the optimal feature subset
-common_features = intersect(results$optVariables[1:50], top_attrs[1:50])
+common_features = intersect(RFE_features$optVariables[1:50], InfoGain_features[1:50])
 
-top_features = list(results$optVariables[1:10], top_attrs[1:10], FS_features[1:10], common_features[1:10])
-algorithms = list('RFE', 'Information Gain', 'Fischer Score', 'RFE intersection Information Gain')
-for(i in length(top_features)){
-  cat("\nTop 10 features of ",algorithms[i], ":")
+top_features = list(RFE_features$optVariables[1:10], InfoGain_features[1:10], FS_features[1:10], LASSO_features[1:10],
+                    CM_features[1:10], common_features[1:10])
+algorithms = list('RFE', 'Information Gain', 'Fischer Score', 'LASSO', 
+                  'Correlation Matrix', 'RFE intersection Information Gain')
+for(i in 1:length(top_features)){
+  algorithm = as.character(algorithms[[i]]) 
+  cat("\nTop 10 features of ",algorithm, ":")
   # XGBoost
   cat("\nTop 10 features:")
   set.seed(42)
-  top_10_features = top_features[i]
+  top_10_features = as.vector(top_features[[i]])
   print(top_10_features)
   x_train_top = as.matrix(x_train[, top_10_features])
   x_test_top = as.matrix(x_test[, top_10_features])
@@ -419,7 +387,7 @@ for(i in length(top_features)){
   end_time = Sys.time()
   print(end_time-start_time)
   y_pred_top = predict(regressor, xgb_test)
-  print(head(predictions))
+  
   
   print(MAE(y_pred = y_pred_top, y_true = y_test))
   print(R2(pred = y_pred_top, obs = y_test))
@@ -457,43 +425,43 @@ for(i in length(top_features)){
   #Random Forest on Features----
   
   # Filter the dataset to include only common features
-  data_filtered <- A[, c("critical_temp", top_10_features)]
-  
+  data_filtered <- df[, c("critical_temp", top_10_features)]
+
   # Set the seed for reproducibility
   set.seed(42)
-  
+
   # Create an index for sampling
   index <- sample(2, nrow(data_filtered), replace = TRUE, prob = c(0.8, 0.2))
-  
+
   # Create training and test sets
   train <- data_filtered[index == 1, ]
   test <- data_filtered[index == 2, ]
-  
-  # Build the random forest model
-  T1 <- system.time({RFM <- randomForest(critical_temp ~ ., data = train)})
-  cat("\n\nTraining Time:", T1)
-  
-  # Print the model summary
-  print(RFM)
-  
-  # Model Accuracy
-  T2 <- system.time({Temp_pred <- predict(RFM, test)})
-  result <- data.frame(test$critical_temp, Temp_pred)
-  cat("\n\nPrediction Time:", T2)
-  print(result)
-  plot(result)
-  
-  # R-square
-  r_squared <- R2(Temp_pred, test$critical_temp)
-  cat("\nR2:", r_squared)
-  
-  # RMSE
-  rmse <- rmse(Temp_pred, test$critical_temp)
-  cat("\nRMSE:", rmse)
-  
-  # MAE
-  mae <- mae(Temp_pred, test$critical_temp)
-  cat("\nMAE:", mae)
+  # 
+  # # Build the random forest model
+  # T1 <- system.time({RFM <- randomForest(critical_temp ~ ., data = train)})
+  # cat("\n\nTraining Time:", T1)
+  # 
+  # # Print the model summary
+  # print(RFM)
+  # 
+  # # Model Accuracy
+  # T2 <- system.time({Temp_pred <- predict(RFM, test)})
+  # result <- data.frame(test$critical_temp, Temp_pred)
+  # cat("\n\nPrediction Time:", T2)
+  # print(result)
+  # plot(result)
+  # 
+  # # R-square
+  # r_squared <- R2(Temp_pred, test$critical_temp)
+  # cat("\nR2:", r_squared)
+  # 
+  # # RMSE
+  # rmse <- RMSE(Temp_pred, test$critical_temp)
+  # cat("\nRMSE:", rmse)
+  # 
+  # # MAE
+  # mae <- MAE(Temp_pred, test$critical_temp)
+  # cat("\nMAE:", mae)
   
   
   #MARS on Features selected----
@@ -519,10 +487,10 @@ for(i in length(top_features)){
   cat("Prediction Time:", T2)
   
   # Calculate performance metrics (e.g., RMSE) for the predictions
-  RMSE <- rmse(predictions, test_data$critical_temp)
+  RMSE <- RMSE(predictions, test_data$critical_temp)
   cat("\nRoot Mean Squared Error (RMSE):", RMSE)
   
-  MAE <- mae(predictions, test_data$critical_temp)
+  MAE <- MAE(predictions, test_data$critical_temp)
   cat("\nMAE:", MAE)
   
   r_square <- R2(predictions, test_data$critical_temp)
@@ -530,9 +498,9 @@ for(i in length(top_features)){
   
   
   
-  # Support Vector Regression Top 10 RFE and InfoGain----
+  # Support Vector Regression Top 10 features----
   
-  data_filtered <- data[, c("critical_temp", top_10_features)]
+  data_filtered <- df[, c("critical_temp", top_10_features)]
   
   X <- data_filtered[, -ncol(data_filtered)]  # All columns except the last one
   y <- data_filtered[, ncol(data_filtered)]   # Last column
@@ -588,84 +556,56 @@ for(i in length(top_features)){
 
 
 # K-Fold cross validation----
-# rmse = c()
-# r2 = c()
-# mae = c()
-# cv = lapply(folds, function(x) {
-#   x_kf_train = as.matrix(subset(df, select = -critical_temp)[-x, ])
-#   x_kf_test = as.matrix(subset(df, select = -critical_temp)[x, ])
-#   y_kf_train = as.matrix(df$critical_temp[-x])
-#   y_kf_test = as.matrix(df$critical_temp[x])
-#   xgb_train = xgb.DMatrix(data = x_kf_train, label = y_kf_train)
-#   xgb_test = xgb.DMatrix(data = x_kf_test, label = y_kf_test)
-#   watchlist = list(train=xgb_train, test=xgb_test)
-#   regressor = xgb.train(data = xgb_train, nrounds = 200, max.depth = 3,
-#                         watchlist = watchlist)
-#   y_pred = predict(regressor, xgb_test)
-#   #mape = MAE(y_pred = y_pred, y_true = y_kf_test, epsilon)
-#   non_zero_indices <- y_kf_test != 0
-#   y_kf_test_filtered <- y_kf_test[non_zero_indices]
-#   y_pred_filtered <- y_pred[non_zero_indices]
-#   rmse = append(rmse, RMSE(y_pred_filtered, y_kf_test_filtered))
-#   r2 = append(r2, R2(y_pred_filtered, y_kf_test_filtered))
-#   mae = append(mae, MAE(y_pred_filtered, y_kf_test_filtered))
-#   print(rmse[length(rmse)])
-#   print(r2[length(r2)])
-#   print(mae[length(mae)])
-#   Sys.sleep(3)
-# })
-# 
-# print(mean(rmse))
-# print(mean(r2))
-# print(mean(mae))
+rmse = c()
+r2 = c()
+mae = c()
+cv = lapply(folds, function(x) {
+  x_kf_train = (subset(df, select = -critical_temp)[-x, ])
+  x_kf_test = (subset(df, select = -critical_temp)[x, ])
+  y_kf_train = (df$critical_temp[-x])
+  y_kf_test = (df$critical_temp[x])
+  # xgb_train = xgb.DMatrix(data = x_kf_train, label = y_kf_train)
+  # xgb_test = xgb.DMatrix(data = x_kf_test, label = y_kf_test)
+  # watchlist = list(train=xgb_train, test=xgb_test)
+  # regressor = xgb.train(data = xgb_train, nrounds = 200, max.depth = 3,
+  #                       watchlist = watchlist)
+  svr_model <- svm(y_kf_train ~ ., data = data.frame(cbind(y_kf_train, x_kf_train)), kernel = "radial", cost = 1)
+  y_pred = predict(regressor, xgb_test)
+  #mape = MAE(y_pred = y_pred, y_true = y_kf_test, epsilon)
+  non_zero_indices <- y_kf_test != 0
+  y_kf_test_filtered <- y_kf_test[non_zero_indices]
+  y_pred_filtered <- y_pred[non_zero_indices]
+  rmse = append(rmse, RMSE(y_pred_filtered, y_kf_test_filtered))
+  r2 = append(r2, R2(y_pred_filtered, y_kf_test_filtered))
+  mae = append(mae, MAE(y_pred_filtered, y_kf_test_filtered))
+  print(rmse[length(rmse)])
+  print(r2[length(r2)])
+  print(mae[length(mae)])
+  Sys.sleep(3)
+})
+
+print(mean(rmse))
+print(mean(r2))
+print(mean(mae))
 
 
 
 
 
 #PCA----
-# Load the necessary library
 library(stats)
-
-# Load your dataset (train.csv)
 data <- read.csv("train.csv")
-
-# Extract column names
 column_names <- colnames(data)
-
-# Standardize the data
 scaled_data <- scale(data)
-
-# Perform PCA
 pca_result <- prcomp(scaled_data, center = TRUE, scale. = TRUE)
-
-# Get the explained variance
 explained_variance <- pca_result$sdev^2 / sum(pca_result$sdev^2)
-
-# Print the explained variance for each principal component along with column names
 cat("Explained Variance for Principal Components:\n")
 for (i in 1:length(explained_variance)) {
-  cat( " (", column_names[i], "): ", explained_variance[i], "\n")
+  cat("PC", i, " (", column_names[i], "): ", explained_variance[i], "\n")
 }
-
-# You can also plot the cumulative explained variance
 cumulative_variance <- cumsum(explained_variance)
 plot(cumulative_variance, type = "b", xlab = "Number of Principal Components", ylab = "Cumulative Explained Variance", main = "Cumulative Explained Variance Plot")
-
-# Calculate cumulative explained variance
 cumulative_variance <- cumsum(explained_variance)
-
-# Find the first PC where cumulative variance exceeds 0.9
 desired_variance <- 0.9
-selected_pc <- which(cumulative_variance >= desired_variance)[1]
-
-# Print the result
+selected_pc <- which(cumulative_variance >= desired_variance)
 cat("The first principal component where cumulative variance exceeds 0.9 is PC", selected_pc, "\n")
-
-
-# Print column names mapped to the loadings (weightage) of their principal components
-# cat("\nLoadings (Weightage) of Principal Components for Each Column:\n")
-# for (i in 1:length(column_names)) {
-# cat("Column: ", column_names[i], "\n")
-# cat("Loadings (Weightage): ", pca_result$rotation[i,], "\n")
-# }
